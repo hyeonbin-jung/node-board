34cbf0c5da4aedbe31a331236bfcfde9d81c5b27
hyeonbin-jung <reno123@nate.com>
2021-06-17 22:51:37 +0900

    .

---
+++ middlewares/main_menu.js
@@ -0,0 +1,16 @@
+/**
+* 메인메뉴 middleware 
+* 
+*/
+module.exports.mainMenu = function(req, res, next) {
+	res.locals.mainMenu = [
+		{name : '질문과 답변', url : '/board/list/qna'},
+		{name : '자유게시판', url : '/board/list/freetalk'},
+		{name : '회원가입', url : '/member/join'},
+		{name : '로그인', url : '/member/login'},
+		
+	];
+	
+	next();
+};
+

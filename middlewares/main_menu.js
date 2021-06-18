ade5df45378ae2a11726527a6bbbcd166f1fd55b
hyeonbin-jung <reno123@nate.com>
2021-06-18 20:14:41 +0900

    스타일, 로그인/로그아웃 메뉴

--- middlewares/main_menu.js
+++ middlewares/main_menu.js
@@ -5,11 +5,7 @@
 module.exports.mainMenu = function(req, res, next) {
 	res.locals.mainMenu = [
 		{name : '질문과 답변', url : '/board/list/qna'},
-		{name : '자유게시판', url : '/board/list/freetalk'},
-		{name : '회원가입', url : '/member/join'},
-		{name : '로그인', url : '/member/login'},
-		{name : '로그아웃', url : '/member/logout'},
-		
+		{name : '자유게시판', url : '/board/list/freetalk'},		
 	];
 	
 	next();
